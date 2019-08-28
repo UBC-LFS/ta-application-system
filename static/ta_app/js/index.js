9aa24c618145fadfa9cf3f415adce516fe5d06bd
austinkwok1997 <austin.kwok@protonmail.com>
2019-08-28 11:56:15 -0700

    made changes to sidebar for admininistrator and instructors

--- static/ta_app/js/index.js
+++ static/ta_app/js/index.js
@@ -1,36 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
   console.log("js ready");
 
-/*
-
-if (window.location.href.indexOf("home") > -1) {
-    document.getElementById('home').classList.add("active");
-} else {
-document.getElementById('home').classList.remove("active");
-}
 
 
-if (window.location.href.indexOf("administrators") > -1) {
-if (document.getElementById('admin')) {
-  document.getElementById('admin').classList.add("active");
-}
-} else {
-if (document.getElementById('admin')) {
-  document.getElementById('admin').classList.remove("active");
-}
-}
+  if (window.location.href.indexOf("administrators") > -1) {
+    document.getElementById('admin').classList.add("active");
+  } else {
+    if (document.getElementById('admin')) {
+      document.getElementById('admin').classList.remove("active");
+    }
+  }
 
-if (window.location.href.indexOf("hr") > -1) {
-if (document.getElementById('hr')) {
-  document.getElementById('hr').classList.add("active");
-}
-} else {
-if (document.getElementById('hr')) {
-  document.getElementById('hr').classList.remove("active");
-}
-}
+  if (window.location.href.indexOf("instructors") > -1) {
+    document.getElementById('instructor').classList.add("active");
+  } else {
+    if (document.getElementById('instructor')) {
+      document.getElementById('instructor').classList.remove("active");
+    }
+  }
 
-*/
-
+  if (window.location.href.indexOf("students") > -1) {
+    document.getElementById('student').classList.add("active");
+  } else {
+    if (document.getElementById('student')) {
+      document.getElementById('student').classList.remove("active");
+    }
+  }
 
 });
