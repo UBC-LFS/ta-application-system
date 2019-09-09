eb3b6a61e4fddda64bf5803fa235c9c647f2608a
exponentian <exponentian.oh@gmail.com>
2019-09-09 08:48:51 -0700

    changed admin url to fix edit job app

--- administrators/urls.py
+++ administrators/urls.py
@@ -31,8 +31,7 @@ urlpatterns = [
     path('applications/offered/send_email/', views.offered_applications_send_email, name='offered_applications_send_email'),
     path('applications/<str:type>/', views.type_applications, name='type_applications'),
     path('sessions/<str:session_slug>/jobs/<str:job_slug>/offer_job/', views.offer_job, name='offer_job'),
-
-    #path('sessions/<str:session_slug>/jobs/<str:job_slug>/edit_job_application/', views.edit_job_application, name='edit_job_application'),
+    path('sessions/<str:session_slug>/jobs/<str:job_slug>/edit_job_application/', views.edit_job_application, name='edit_job_application'),
     path('applications/<str:app_slug>/', views.show_application, name='show_application'),
     path('applications/offered/email_history', views.email_history, name='email_history'),
     path('applications/', views.applications, name='applications'),
