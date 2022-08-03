c65af60738e6d509d53acbb7f0e1a25dabbb860a
exponentian <exponentian.oh@gmail.com>
2022-08-03 16:12:21 -0700

    added default_auto_field in apps

--- scheduler/apps.py
+++ scheduler/apps.py
@@ -1,7 +1,9 @@
 from django.apps import AppConfig
 from django.conf import settings
 
+
 class SchedulerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'scheduler'
 
     def ready(self):
