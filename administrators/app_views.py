ef75c4903572c3fdc1b417304a782ac327923d79
exponentian <exponentian.oh@gmail.com>
2026-04-10 12:02:35 -0700

    fixed the report for all apps

--- administrators/app_views.py
+++ administrators/app_views.py
@@ -153,8 +153,15 @@ def download_all_apps(request):
     data = []
     for app in apps:
         year = app.job.session.year
+        
+        pref_candi = ''
+        if userApi.profile_exists(app.applicant):
+            pref_candi = 'YES' if app.applicant.profile.preferred_candidate_status else ''
 
         latest_status = app.applicationstatus_set.last()
+        assigned_display = latest_status.get_assigned_display()
+        if assigned_display == 'None':
+            assigned_display = 'Applied'
 
         confi_info_expiry_status = userApi.get_confidential_info_expiry_status(app.applicant)
         confi = []
@@ -170,10 +177,6 @@ def download_all_apps(request):
                     app_reset.user
                 ))
         
-        pref_candi = ''
-        if userApi.profile_exists(app.applicant):
-            pref_candi = 'YES' if app.applicant.profile.preferred_candidate_status else ''
-        
         data.append({
             'LFS TA': 'YES' if userApi.get_lfs_ta(app.applicant, year) else '',
             'Preferred Candidate': pref_candi,
@@ -181,7 +184,8 @@ def download_all_apps(request):
             'Term': app.job.session.term.code,
             'Job': '{0} {1} {2}'.format(app.job.course.code.name, app.job.course.number.name, app.job.course.section.name),
             'Applicant': app.applicant.get_full_name(),
-            'Latest Status': '{0} (Assigned Hours: {1})'.format(latest_status.get_assigned_display(), latest_status.assigned_hours),
+            'Latest Status': assigned_display,
+            'Assigned Hours': latest_status.assigned_hours,
             'Classification': '{0} {1}'.format(app.classification.year, app.classification.name) if app.classification else '',
             'GTA Info': userApi.get_gta_flag(app.applicant),
             'Confidential Info': ', '.join(confi),
